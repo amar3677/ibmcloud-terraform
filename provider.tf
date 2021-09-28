c1b6c73531271fe86bbdeef86a58556aea3855a7
amar3677 <63534566+amar3677@users.noreply.github.com>
2021-09-28 16:59:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,19 +1,15 @@
 variable "ibmcloud_api_key" {}
 variable "region" {}
-variable "ssh_key" {}
+variable "cluster_name"
+variable "datacenter"
+variable "hardware"
+variable "poolsize"
+variable "machine_type"
+variable "public_vlan_id"
+variable "private_vlan_id"
+variable "kube_version"
 
 provider "ibm" {
     ibmcloud_api_key = var.ibmcloud_api_key
     region = var.region
     }
-
-resource "ibm_container_cluster" "cluster" {
-  name              = var.cluster_name
-  datacenter        = var.datacenter
-  hardware          = var.hardware
-  default_pool_size = var.poolsize
-  machine_type      = var.machine_type
-  public_vlan_id    = var.public_vlan_id
-  private_vlan_id   = var.private_vlan_id
-  kube_version      = var.kube_version
-}
